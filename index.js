00afa178f086e6999957058f8579ffe2f197ea5b
stephink <stephin0398@gmail.com>
2020-06-19 00:30:31 +0530

    get cll

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 app.use(express.json());
+app.use(cors());
 
 app.get('/concat', (req, res) => {
   const firstname = req.param('firstName');
